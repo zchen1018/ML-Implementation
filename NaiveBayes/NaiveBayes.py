4817128258cba07f64e42292e8d9e2a03b24741f
Zhuangye Chen <zhuangye@stat.wisc.edu>
2016-02-15 18:52:58 -0600

    Update functions for the learning output from data

--- NaiveBayes/NaiveBayes.py
+++ NaiveBayes/NaiveBayes.py
@@ -25,28 +25,61 @@ def load_data(file):
 
 	return rows_matrix, variable_names, variable_column
 
-# calculate the number of the occurence for a given variable
+# calculate the number of the occurence for a given variable 
+# using Laplace estimates with pseudocounts = 1
 # return a dictionary : {var1: num1, var2: num2, ...}
-def countX(data, var_name, var_values_dict, var_names_list) :
+def countX(data, var_name, var_values_dict, var_names_list, pseudocount = 1) :
 	ind = var_names_list.index(var_name) # get the index for the variable
 	var_values = var_values_dict[var_name] # all values of the variable
-	output = dict((ele, 0) for ele in var_values) # create a list with variable values as keys
+	output = dict((ele, pseudocount) for ele in var_values) # create a list with variable values as keys
 	
 	for obs in data:
 		output[obs[ind]] += 1
 
 	return output
 
+# Learning the frequency for each variable
+# output: a list of dictionary, each is the adjusted frequency for each variable value
+def count_frequency(data, var_values_dict, var_names_list) :
+	freq_result = [] # a list for frequency of each variable
+	for var in var_names_list[:-1]: # not include the class variable
+		freq_result.append(countX(data, var, var_values_dict, var_names_list))
 
-# 
-# def bayes_learning(training_set): # training_set is a list of rows
-# 	class_labels = set([x['class'] for x in training_set)
-# 	# a list to store the learning result 
-# 	# each element is of the form [label, {variable_name: var}, conditional_prob]
-# 	learning_result = []
-# 	for label in class_labels:
-# 		data_of_class = [x for x in training_set if x['class'] == label]
-# 		for var_name in data_of_class[0].keys()[:-1]:
+	return freq_result
+
+# Estimate the probabilty for the class variable
+def class_prob(data, var_values_dict, var_names_list):
+	count = countX(data, 'class', var_values_dict, var_names_list, 0) # no pseudocount
+	sample_size = float(sum(count.values()))
+	for x in count:
+		count[x] /= sample_size
+	return count
+
+# Estimate conditional probability P(X = x | y) for each x
+# output: a list of conditioanl prob estimate for each class in target variable
+def conditional_prob(data, var_values_dict, var_names_list):
+	freq_list = []
+	name = var_names_list[-1] # Assume that the target variable is always the last variable in the data
+	target = var_values_dict[name]
+	for target_var in target:
+		# Find the data of the specific class value
+		class_data = []
+		for obs in data:
+			if obs[-1] == target_var:
+				class_data.append(obs)
+
+		freq_list.append(count_frequency(class_data, var_values_dict, var_names_list))
+
+	for freq in freq_list: # freq is a list of frequency dictionaries for each variable
+		for x in freq: # x is a frequence dictionary for a variable
+	 		group_sum = float(sum(x.values())) 
+	 		for temp in x:
+	 			x[temp] /= group_sum # update the requence with the probability
+
+	return freq_list
+
+# The output of class_prob() and conditional_prob() are basically the learning result
+# from the data
 
 	
 
